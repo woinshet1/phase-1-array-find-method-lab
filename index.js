739ce7ccce6d9f6214a4920531dc1672f00ca173
woinshet1 <woinshet@DESKTOP-VDL1QKS.localdomain>
2022-03-31 20:37:30 -0400

    complet assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(record) {
+    let d = record.find(function(winning_year) {
+        return winning_year.result === 'W';
+   })
+   return !!d ? d.year : d;
+}
